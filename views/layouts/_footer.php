4c670a428585573b41b10e3ba579ea355ef66b9f
TSAdigital <s.tarhanov174@gmail.com>
2025-04-27 18:29:40 +0500

    Обрезка текста для мобильной версии

--- views/layouts/_footer.php
+++ views/layouts/_footer.php
@@ -7,7 +7,7 @@ use yii\bootstrap5\Html;
     <div class="container-fluid">
         <div class="row text-muted">
             <div class="col-8 text-start">
-                <p class="mb-0">
+                <p class="mb-0 text-truncate">
                     Разработано в <?= Html::a('TSAdigital', 'https://tsa-digital.ru', ['target' => '_blank']) ?>
                     на базе <?= Html::a('Yii2', 'https://www.yiiframework.com', ['target' => '_blank']) ?> и <?= Html::a('AdminKit', 'https://adminkit.io', ['target' => '_blank']) ?>
                 </p>
@@ -15,7 +15,7 @@ use yii\bootstrap5\Html;
             <div class="col-4 text-end">
                 <ul class="list-inline">
                     <li class="list-inline-item">
-                        Версия 1.0.1
+                        Версия 1.0.2
                     </li>
                 </ul>
             </div>
